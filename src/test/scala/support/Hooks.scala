efd0b61fb196837ef77b91679d1c307fba0777b1
Andrew Boyce <andrew.boyce96@hotmail.com>
2025-06-19 12:54:29 +0100

    first commit

---
+++ src/test/scala/support/Hooks.scala
@@ -0,0 +1,25 @@
+package support
+
+import io.cucumber.scala.{EN, ScalaDsl}
+import org.openqa.selenium.chrome.{ChromeDriver, ChromeOptions}
+import utils.ScreenCapture.takeScreenshot
+
+
+class Hooks extends ScalaDsl with EN {
+
+  val options = new ChromeOptions()
+  options.addArguments("--headless=new")
+
+  Before {
+    println("Launching browser before scenario...")
+    DriverManager.driver = new ChromeDriver(options)
+    DriverManager.driver.manage().window().maximize()
+  }
+
+  After {
+    takeScreenshot(DriverManager.driver, prefix = "CucumberTest")
+    println("Closing browser after scenario...")
+    DriverManager.driver.quit()
+//    DriverManager.driver = null
+  }
+}
---
+++ src/test/scala/utils/ScreenCapture.scala
@@ -0,0 +1,30 @@
+package utils
+
+import org.openqa.selenium.{OutputType, TakesScreenshot, WebDriver}
+import org.openqa.selenium.io.FileHandler
+
+import java.io.File
+import java.text.SimpleDateFormat
+import java.util.Date
+
+object ScreenCapture {
+// THIS IS EMPTY
+def takeScreenshot(
+                    driver: WebDriver,
+                    basePath: String = "/Users/andrew.boyce/Documents/Screenshots",
+                    prefix: String = ""
+                  ): Unit = {
+  // Set up params with default options
+  // Create a timestamp
+  val timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date())
+  // Take a screenshot
+  val screenshotFile = driver.asInstanceOf[TakesScreenshot].getScreenshotAs(OutputType.FILE)
+  // Build a full path based on input strings and timestamp
+  val fullPath = s"${basePath}/${prefix}_${timeStamp}.png"
+  // Save the screenshot to the destination
+  FileHandler.copy(screenshotFile, new File(fullPath))
+  // Print out where the screenshot has been saved locally
+  println(s"Screenshot saved to: $fullPath")
+}
+
+}
